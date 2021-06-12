f14b32d5069ae973cf552053def4681f013dfa1b
rahulAtGit <rahul.rvce.is@gmail.com>
2021-06-12 11:56:58 +0530

    router query

--- pages/layout-intrinsic.js
+++ pages/layout-intrinsic.js
@@ -1,18 +1,25 @@
-import Image from 'next/image'
-import ViewSource from '../components/view-source'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import ViewSource from "../components/view-source";
 
-const Intrinsic = () => (
-  <div>
-    <ViewSource pathname="pages/layout-intrinsic.js" />
-    <h1>Image Component With Layout Intrinsic</h1>
-    <Image
-      alt="Mountains"
-      src="/mountains.jpg"
-      layout="intrinsic"
-      width={700}
-      height={475}
-    />
-  </div>
-)
+const Intrinsic = () => {
+  const router = useRouter();
+  const { quality, optmize } = router.query;
+  return (
+    <div>
+      <ViewSource pathname="pages/layout-intrinsic.js" />
+      <h1>Image Component With Layout Intrinsic 2</h1>
+      <Image
+        alt="Mountains"
+        src="/mountains.jpg"
+        layout="intrinsic"
+        width={700}
+        height={475}
+        quality={quality ? quality : 100}
+        unoptimized={!!optmize}
+      />
+    </div>
+  );
+};
 
-export default Intrinsic
+export default Intrinsic;
