ccc8769236441d382420302eb96d69b812322a5f
rahulAtGit <rahul.rvce.is@gmail.com>
2021-06-12 12:06:57 +0530

    Quality

--- pages/layout-intrinsic.js
+++ pages/layout-intrinsic.js
@@ -8,14 +8,14 @@ const Intrinsic = () => {
   return (
     <div>
       <ViewSource pathname="pages/layout-intrinsic.js" />
-      <h1>Image Component With Layout Intrinsic 3</h1>
+      <h1>Image Component With Layout Intrinsic 4</h1>
       <Image
         alt="Mountains"
         src="/mountains.jpg"
         layout="intrinsic"
         width={700}
         height={475}
-        unoptimized={true}
+        quality={quality ? quality : 100}
       />
     </div>
   );
